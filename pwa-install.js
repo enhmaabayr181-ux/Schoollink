7e55ce8dfbb19c2439e4c266fadd454a9e23ac8b
ema___stgramm <enhmaabayr181@gmail.com>
2026-08-21 23:20:54 +0800

    Load billing module

--- pwa-install.js
+++ pwa-install.js
@@ -69,3 +69,4 @@ shLoadLateScript('/teacher-sharing.js','schoolhub-teacher-sharing');
 shLoadLateScript('/portfolio-sharing-patch.js','schoolhub-portfolio-sharing');
 shLoadLateScript('/visual-cards.js','schoolhub-visual-cards');
 shLoadLateScript('/launch-polish.js','schoolhub-launch-polish');
+shLoadLateScript('/billing-live.js','schoolhub-billing');
